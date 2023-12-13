177693e3e2aff8f5a22d15251cc9a603073644c3
Uheder Macedo <dev.usmacedo@outlook.com>
2023-12-13 15:44:15 -0300

    added two urls

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, url_for
+from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return '<p>Hello, World!</p>'
+@app.route('/<name>')
+def name_page(name):
+  return f'<p>Hello, {escape(name.title())}!</p>'
+
+@app.route('/home')
+def home():
+    return '<h1>This is your home page</h1>'
+
+with app.test_request_context():
+   print(url_for('home'))
